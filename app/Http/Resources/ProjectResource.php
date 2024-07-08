a394e193a9bbde7906d55aa26ccbb615a3330c3b
Muhammad Saqlain <msaqlain6666@gmail.com>
2024-07-08 23:13:07 +0500

    wip

--- app/Http/Resources/ProjectResource.php
+++ app/Http/Resources/ProjectResource.php
@@ -15,7 +15,6 @@ class ProjectResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        
         return [
             'id' => $this->id ?? null,
             'name' => $this->name ?? null,
